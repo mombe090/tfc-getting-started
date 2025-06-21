cbd6811084a660171e05512272e899415b96e7ac
Mamadou Yaya DIALLO <yayamombeya090@gmail.com>
2025-06-21 16:29:09 -0400

    add SNS topic resource and enable S3 bucket versioning

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
 resource "aws_s3_bucket" "this" {
   bucket = "test-buckets-233444-${var.resource_suffix2}"
 }
+
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_sns_topic" "this" {
+  name = "test-topic-${var.resource_suffix2}"
 }
