857437a2fba7fa6859050966c103ec1f71fde2eb
Mamadou Yaya DIALLO <yayamombeya090@gmail.com>
2025-06-21 15:55:47 -0400

    using github trigger

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "this" {
+  bucket = "test-buckets-233444-${var.resource_suffix2}"
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
